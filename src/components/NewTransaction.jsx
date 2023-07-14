e7bed4cd7462baedfa06261e319f63f6f2830744
Vinayak Chittora <vinayakchittora@gmail.com>
2023-07-14 11:21:27 +0530

    files added

---
+++ src/components/NewTransaction.jsx
@@ -0,0 +1,39 @@
+import styled from "@emotion/styled";
+import { Box, Button, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+const Container = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  & > h5,
+  & > div,
+  & > button {
+    margin-top: 30px;
+  }
+`;
+
+const NewTransaction = ({setTransactions}) => {
+
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState();
+
+  const addTransaction = () => {
+    const transaction = {
+      id: Math.floor(Math.random() * 100000),
+      text: text,
+      amount: +amount
+    }
+    setTransactions(prevState => [transaction, ...prevState]);
+  }
+
+  return (
+    <Container>
+      <Typography variant="h5">New Transaction</Typography>
+      <TextField label="Enter Expense" onChange={(e) => setText(e.target.value)}/>
+      <TextField label="Enter Amount" onChange={(e) => setAmount(e.target.value)}/>
+      <Button variant="contained" onClick={() => addTransaction()}>Add Transaction</Button>
+    </Container>
+  );
+};
+
+export default NewTransaction;
